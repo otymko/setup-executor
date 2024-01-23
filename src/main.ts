6a589c18f4eb8ef528a6d77299433dfaf591077e
olegtymko <olegtymko@yandex.ru>
2024-01-23 21:07:28 +0700

    init repo by template "typescript-action"

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import * as core from '@actions/core'
+
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+export async function run(): Promise<void> {
+  core.debug('sample')
+  console.log('sample')
+}
